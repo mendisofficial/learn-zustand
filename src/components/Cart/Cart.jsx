e3bdce30d619c29e25e027a6f1377b36b6e3694d
Chathusha Mendis <chathushamendis@icloud.com>
2024-12-18 07:04:32 +0530

    Refactor cart and product list components to use Zustand for state management

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,18 +1,24 @@
-const Cart = ({ cart, setCart }) => {
-    return (
-        <div>
-            <h2>Cart</h2>
-            {cart.map((product) => (
-                <div key={product.id}>
-                    <span>{product.name}</span>
-                    <button onClick={() => setCart(cart.filter((productItem) => productItem.id !== product.id))}>Remove</button>
-                </div>
-            ))}
-            {cart.length > 0 && (
-                <button onClick={() => setCart([])}>Clear Cart</button>
-            )}
+import { useCartStore } from "../../store/cart-store";
+
+const Cart = () => {
+  const { cart, removeFromCart, clearCart } = useCartStore((state) => ({
+    cart: state.cart,
+    removeFromCart: state.removeFromCart,
+    clearCart: state.clearCart,
+  }));
+
+  return (
+    <div>
+      <h2>Cart</h2>
+      {cart.map((product) => (
+        <div key={product.id}>
+          <span>{product.name}</span>
+          <button onClick={() => removeFromCart(product.id)}>Remove</button>
         </div>
-    )
-}
+      ))}
+      {cart.length > 0 && <button onClick={clearCart}>Clear Cart</button>}
+    </div>
+  );
+};
 
 export default Cart;
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,15 +1,19 @@
-const ProductList = ({ products, setCart }) => {
-    return (
-        <div>
-            {products?.map((product) => (
-                <div key={product.id}>
-                    <h3>{product.name}</h3>
-                    <p>{product.description}</p>
-                    <button onClick={() => setCart((cart) => [...cart, product])}>Add to cart</button>
-                </div>
-            ))}
+import { useCartStore } from "../../store/cart-store";
+
+const ProductList = ({ products }) => {
+  const addToCart = useCartStore((state) => state.addToCart);
+
+  return (
+    <div>
+      {products?.map((product) => (
+        <div key={product.id}>
+          <h3>{product.name}</h3>
+          <p>{product.description}</p>
+          <button onClick={() => addToCart(product)}>Add to cart</button>
         </div>
-    );
-}
+      ))}
+    </div>
+  );
+};
 
 export default ProductList;
